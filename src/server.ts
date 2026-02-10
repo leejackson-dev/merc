7c50ba621d2e03365f038c7ef7d1b51d208eef11
leejackson-dev <lee.jackson@riverai.co.uk>
2026-02-10 16:52:18 +0000

    add cors

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,19 @@ import express from "express";
 import { uploadRouter } from "./routes/upload.js";
 import { askRouter } from "./routes/ask.js";
 import { exportExcelRouter } from "./routes/exportExcel.js";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Content-Disposition"], // IMPORTANT for file downloads
+  }),
+);
 
 app.get("/", (_req, res) => res.send("OK"));
 
